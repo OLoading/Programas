a5ce759056617898b966d93ec6a3da8cbff6d196
OLoading <luander2010@outlook.com>
2023-07-10 19:57:58 -0300

    Object

---
+++ A.js
@@ -0,0 +1,17 @@
+Object = 
+{
+    chave: 5,
+    lista: [1,2,3,4,5],
+    niggers: "Niggers",
+    Thiago: "NegoBrancasso",
+        A: 
+            {
+                1: "um",
+                2: "dois",
+                3: "tres"
+            }
+}
+
+// delete Object.lista
+
+console.log(Object)
